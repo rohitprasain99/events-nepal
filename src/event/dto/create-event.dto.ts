d65c80fb40e8fd7810f80869df10b50fc0d6aef9
Rohit Prasain <workstuffrohit@gmail.com>
2024-09-03 17:16:03 +0100

    chore: add class validator and transformer package

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateEventDto {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   location: string;
 }
